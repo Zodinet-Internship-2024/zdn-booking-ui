8c373031d7822b1ce8e6f2d7161a5131cbc2a324
zdn-duc-nguyen <duc.nguyen@zodinet.tech>
2024-06-06 10:53:46 +0700

    chore: add alerts and accent color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,34 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        accent: {
+          100: "#FBF9FE",
+          200: "#F5F2FD",
+          300: "#E3DBF9",
+          400: "#D1C2F9",
+          500: "#B7A3F0",
+          600: "#967DDD",
+        },
+        alerts: {
+          green: {
+            light: "#6AB557",
+            DEFAULT: "#EDF6EB",
+          },
+          red: {
+            light: "#FDEBE7",
+            DEFAULT: "#EB5339",
+          },
+          yellow: {
+            light: "#FFF8E9",
+            DEFAULT: "#FAB424",
+          },
+          blue: {
+            light: "#E7F5FF",
+            DEFAULT: "#0C99FF",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
