2d5e5c098e106fab892b45af2d616e91203ae8d2
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-06 10:54:35 +0700

    chore: add primary and neutral colors config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    100: '#F6FCF1',
+                    200: '#EBF8E0 ',
+                    300: '#DAEBCC',
+                    400: '#C4DCB1',
+                    500: '#A2C287',
+                    600: '#7FA65F',
+                },
+                neutral: {
+                    DEFAULT: '#FFFFFF',
+                    100: '#F1F3F6',
+                    200: '#C8CED9',
+                    300: '#8292AA',
+                    400: '#384252',
+                    500: '#242B35',
+                    600: '#40423F ',
+                },
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
