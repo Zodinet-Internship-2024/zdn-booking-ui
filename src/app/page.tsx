a3cb1664450cc57afb2d80e4c17ef56caeb77a9f
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-06 11:23:23 +0700

    style: set up base css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-alerts-green"></main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+            <h1>ascsac</h1>
+        </main>
     );
 }
