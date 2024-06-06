b87ca9d5e2785469274d2c45b49b51f766d5c343
zdn-toan-tran <toan.tran@zodinet.tech>
2024-06-06 10:59:21 +0700

    fix: fix merge tailwind config errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,36 +26,34 @@ const config: Config = {
                     500: '#242B35',
                     600: '#40423F ',
                 },
+                accent: {
+                    100: '#FBF9FE',
+                    200: '#F5F2FD',
+                    300: '#E3DBF9',
+                    400: '#D1C2F9',
+                    500: '#B7A3F0',
+                    600: '#967DDD',
+                },
+                alerts: {
+                    green: {
+                        light: '#6AB557',
+                        DEFAULT: '#EDF6EB',
+                    },
+                    red: {
+                        light: '#FDEBE7',
+                        DEFAULT: '#EB5339',
+                    },
+                    yellow: {
+                        light: '#FFF8E9',
+                        DEFAULT: '#FAB424',
+                    },
+                    blue: {
+                        light: '#E7F5FF',
+                        DEFAULT: '#0C99FF',
+                    },
+                },
             },
-            colors: {
-        accent: {
-          100: "#FBF9FE",
-          200: "#F5F2FD",
-          300: "#E3DBF9",
-          400: "#D1C2F9",
-          500: "#B7A3F0",
-          600: "#967DDD",
-        },
-        alerts: {
-          green: {
-            light: "#6AB557",
-            DEFAULT: "#EDF6EB",
-          },
-          red: {
-            light: "#FDEBE7",
-            DEFAULT: "#EB5339",
-          },
-          yellow: {
-            light: "#FFF8E9",
-            DEFAULT: "#FAB424",
-          },
-          blue: {
-            light: "#E7F5FF",
-            DEFAULT: "#0C99FF",
-          },
-        },
-      },
-      backgroundImage: {
+            backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
