ae19eb8989f40ef0594c60b0b30752a8612da9fe
zdn-triet-nguyen <triet.nguyen@zodinet.tech>
2024-06-17 12:02:14 +0700

    fix: Update next.config to remove unnecessary headers "Why we need this ?"

--- src/app/(auth)/sign-up/SignUpForm.tsx
+++ src/app/(auth)/sign-up/SignUpForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect } from 'react';
-import { Button, Input, message } from 'antd';
+import { Button, Input, message, notification } from 'antd';
 import {
   ArrowLeftOutlined,
   EyeInvisibleOutlined,
