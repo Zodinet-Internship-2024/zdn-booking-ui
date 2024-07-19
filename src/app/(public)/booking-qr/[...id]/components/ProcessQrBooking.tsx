672ffd1e2d2b1df0d4a1646145255bf87ee29803
zdn-toan-tran <toan.tran@zodinet.tech>
2024-07-19 10:07:05 +0700

    feat: redirect to booking ui after login

--- src/app/(public)/booking-qr/[...id]/components/ProcessQrBooking.tsx
+++ src/app/(public)/booking-qr/[...id]/components/ProcessQrBooking.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { updateQrBooking } from '../../api/qrbooking.api';
 type responseBooking = {
@@ -9,11 +9,15 @@ type responseBooking = {
 export default function ProcessQrBooking() {
   const [qrBooking, setQrBooking] = useState<responseBooking>();
   const param = useParams();
-
+  const router = useRouter();
   const idBooking = param.id[0];
   const getQrBooking = async () => {
     const res = await updateQrBooking(idBooking);
     if (res.type === 'Error') {
+      if (res.message === 'Access token is missing') {
+        router.push(`/login?redirect=/booking-qr/${idBooking}`);
+        return;
+      }
       setQrBooking({
         type: res.type,
         message: res.message,
--- src/app/(public)/booking-qr/[...id]/page.tsx
+++ src/app/(public)/booking-qr/[...id]/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import ProcessQrBooking from './components/ProcessQrBooking';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
 export const maxDuration = 60;
-export default function page() {
+
+type BookingQrPageProps = {
+  params: {
+    id: string;
+  };
+};
+export default async function BookingQrPage({
+  params: { id },
+}: BookingQrPageProps) {
+  const session = await getServerSession();
+  console.log('🚀 ~ session:', session);
+
+  if (!session?.user) {
+    redirect(`/login?redirect=/booking-qr/${id}`);
+  }
   return (
     <div>
       <ProcessQrBooking />
